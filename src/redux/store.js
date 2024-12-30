700057749b506a202567049d4fa686a5fcff4422
RensithUdara <rensithudaragonalagoda@gmail.com>
2024-12-30 20:06:38 +0530

    Middlewares using Redux Toolkit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,18 @@
-import {configureStore} from "@reduxjs/toolkit"
+import { configureStore } from "@reduxjs/toolkit"
 import { fruitReducer } from "./features/fruitSection/fruitSlice";
+import logger from "redux-logger";
 
-const store = configureStore({  
+const store = configureStore({
     reducer: {
-        fruit: fruitReducer,    },
+        fruit: fruitReducer,
+    },
+    middleware: (getDefaultMiddleware) => {
+        // console.log(getDefaultMiddleware());
+        // const defaultArray = getDefaultMiddleware();
+        // return [...defaultArray, logger];
+
+        return getDefaultMiddleware().concat(logger);
+    },
 });
 
 export default store;
